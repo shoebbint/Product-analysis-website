2bc9954b452b73b58caa7312f0285b83c52a9778
USER <shoebbint@gmail.com>
2022-04-05 18:12:56 +0600

    almost-done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import CustomerReviews from '../CustomerReviews/CustomerReviews';
 
 import r1 from '../images/r1.png'
@@ -17,7 +18,9 @@ const Home = () => {
                             </h1>
                         </div>
                         <p className='text-start'>Not sure which bike to buy? We'll help you choose the motorcycle based on your budget and performance, mileage requirements. Answer the questions below and let us recommend the bikes that best match your preferences.</p>
-
+                        <div className='btn-div'>
+                            <button className='' type="button" class="btn btn-info  "><Link className='tutorial-btn' to="/about">See tutorial</Link></button>
+                        </div>
 
                     </Col>
                     <Col className='right-col' xs={6} md={6}>
